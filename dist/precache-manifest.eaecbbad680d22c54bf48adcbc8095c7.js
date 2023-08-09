9959e874b9a035ed7647d12f10efba25a61d42c5
henriquecorte21 <henrique.corte@neoprospecta.com>
2023-08-09 20:50:57 -0300

    build

---
+++ dist/precache-manifest.eaecbbad680d22c54bf48adcbc8095c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f52b2c3c26dbc883816",
+    "url": "/css/chunk-7a94c5bf.2102bf8a.css"
+  },
+  {
+    "revision": "96fa11510ebc648f91b6",
+    "url": "/css/chunk-bcb1f2d2.15290f63.css"
+  },
+  {
+    "revision": "21f906dd91a3d93df8fd",
+    "url": "/css/chunk-vendors.d6d7bd72.css"
+  },
+  {
+    "revision": "65eb3506c82f934c3ba9f93a4cc35fbd",
+    "url": "/img/bg.65eb3506.jpg"
+  },
+  {
+    "revision": "833ab9ae02bfdda375c264f5cd18b1f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d274e839ed5cb5a6a91",
+    "url": "/js/app.059b4d49.js"
+  },
+  {
+    "revision": "4f52b2c3c26dbc883816",
+    "url": "/js/chunk-7a94c5bf.512291d8.js"
+  },
+  {
+    "revision": "96fa11510ebc648f91b6",
+    "url": "/js/chunk-bcb1f2d2.08e0f0c2.js"
+  },
+  {
+    "revision": "21f906dd91a3d93df8fd",
+    "url": "/js/chunk-vendors.c9476f48.js"
+  },
+  {
+    "revision": "70feb7fa696313b6ff479cdc2f8fdcf4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
